cf066f4d1e47c7c3f539550337573f915c9757b4
marieblais3 <marieblais3@orange.fr>
2019-12-13 14:27:39 +0100

    page profil utilisateur

--- views/views_connection/viewUser.php
+++ views/views_connection/viewUser.php
@@ -15,68 +15,68 @@
     </style>
     <title>Mon compte</title>
 </head>
-<body>
+<body class="police">
 <?php
 require_once 'views/views_accueil/viewHeader.php';
 ?>
 <div class="body">
     <div class="id_utilisateur">
         <div class="titre_id">
-            <img src="<?=URL?>images/icone_utilisateur.png" id="icone_utilisateur">
             <p id="nom_prenom"><?php echo $account->getName().' '.$account->getFirstName();?></p>
         </div>
         <div class="txt_id">
             <div class="div_txt_id_1">
                 <p class="txt_id_spe">Adresse mail : </p>
-            </div>
-            <div class="div_txt_id_2">
                 <p class="txt_id_php"><?php echo $account->getMailAddress();?></p>
             </div>
             <div class="div_txt_id_1">
-                <p class="txt_id_spe">Genre :</p>
-            </div>
-            <div class="div_txt_id_2">
+                <p class="txt_id_spe">Genre : </p>
                 <p class="txt_id_php">AF</p>
             </div>
             <div class="div_txt_id_1">
-                <p class="txt_id_spe">Taille :</p>
-            </div>
-            <div class="div_txt_id_2">
-                <p class="txt_id_php"><?php echo $account->getHeight();?></p>
+                <p class="txt_id_spe">Taille : </p>
+                <p class="txt_id_php"> <?php echo $account->getHeight();?></p>
             </div>
             <div class="div_txt_id_1">
-                <p class="txt_id_spe">Masse :</p>
-            </div>
-            <div class="div_txt_id_2">
+                <p class="txt_id_spe">Masse : </p>
                 <p class="txt_id_php"><?php echo $account->getWeight();?></p>
             </div>
             <div class="div_txt_id_1">
-                <p class="txt_id_spe">Né le :</p>
-            </div>
-            <div class="div_txt_id_2">
+                <p class="txt_id_spe">Né le : </p>
                 <p class="txt_id_php">10/10/2010</p>
             </div>
         </div>
     </div>
 </div>
-<div class="stats_utilisateur">
-    <img src="<?=URL?>images/test_1.png" class="img_test">
-    <img src="<?=URL?>images/test_2.jpg" class="img_test">
-</div>
 
-<div>
-    <form method="post" action="<?=URL?>account/disconnect">
-        <input type="submit" name="disconnect" value="Se déconnecter">
-    </form>
-</div>
+  <div class="button1">
+      <button class="bouton_resultat" onclick="">Consulter mes résultats</button>
+    </div>
+
+
+      <!--<div class="button2">
+        <button type="submit" class="button_submit" id="modify" name="submit_param" value="<?=$account->getId()?>">
+            Modifier
+          </button>
+        <button class="button_deconnexion" onclick="">Me déconnecter</button>
+    </div>
+*/-->
+
+
 <div>
     <form method="post" action="<?=URL?>account/modify">
         <input type="hidden" name="modify" value="<?=$account->getId()?>">
-        <button type="submit" class="button-submit" id="modify" name="submit_param" value="<?=$account->getId()?>">
-            Modifier
-        </button>
+
+        <input type="submit" name="disconnect" value="Modifier mon profil">
+
     </form>
 </div>
+<div>
+    <form method="post" action="<?=URL?>account/disconnect">
+        <input type="submit" name="disconnect" value="Me déconnecter">
+    </form>
+</div>
+
 
 </body>
 </html>
